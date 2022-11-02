082f47df962007d37f07190f8ebb49e568b1b257
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-11-02 12:54:30 +0800

    feat: updated post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,27 +2,28 @@
 import PostStockTracker from '../models/postStockModel.js';
 
 export const getPosts = async (req, res) => {
+    let { userId } = req.query;
+    console.log(userId);
     try {
-        const postStockFav = await PostStockTracker.find();
-        res.status(200).json(postStockFav);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
+        const postStockFav = await PostStockTracker.find({ userId: userId });
 
+        res.status(200).json({data: postStockFav, success: true, error: '' });
+    } catch (error) {
+        res.status(404).json({ data: {}, success: false, error:'' });
     }
-}
+};
 
 export const createPost = async (req, res) => {
-    const post = req.body;
+    console.log(req.body);
 
-    const newStockFav = new PostStockTracker(post);
     try {
+        const newStockFav = new PostStockTracker(req.body);
         await newStockFav.save();
-        res.status(201).json(newStockFav);
+        res.status(200).json({ status: 200, data: newStockFav, error: '' });
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
-    
-}
+};
 
 export const deletePost = async (req, res) => {
     const { symbol } = req.query;
