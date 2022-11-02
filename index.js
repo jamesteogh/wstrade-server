9a550263b3af478714254e3b6fd2dddc9a7f7d27
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-11-02 21:52:34 +0800

    fix: add Procfile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// import * as dotenv from 'dotenv' 
-// const dotenv = require('dotenv')
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import userRouter from './router/users.js';
 import postRouter from './router/posts.js'
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
+const port = process.env.PORT || 5000;
 const dbName = "Wall_Street_Trade"
 
 // app.use(bodyParser.json({extended: true}));
@@ -16,13 +17,15 @@ app.use(cors());
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/auth', userRouter);
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("Hello World! This is Project 4 server");
+});
 
 // const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}?retryWrites=true&w=majority`
 const CONNECTION_URL = `mongodb+srv://jamesteogh:h5yjq9hd@generalassembly.wwul4.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose
-.connect(CONNECTION_URL, { dbName: dbName })
+.connect(CONNECTION_URL, { dbName })
 .then(()=>{
     console.log('db connected successfully!')
 })
