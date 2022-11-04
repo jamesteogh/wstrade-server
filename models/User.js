3fd3504d0f4e5812705e66868ddaaa4675a8b722
James Teo Gee Hoe <jamesteogh@gmail.com>
2022-11-05 01:40:43 +0800

    fix: tidy up

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
         unique: true,
         match: [
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'please use a valid Email',
+            'please use a valid email',
         ],
     },
 
